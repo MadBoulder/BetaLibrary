e25e73b5718cb005f29e4fcd112272b7ca1f53c2
Juan Gallostra <juangallostra@gmail.com>
2019-06-19 20:25:07 +0200

    Add function to create a map from JSON data

---
+++ load_map.py
@@ -0,0 +1,50 @@
+import numpy as np
+import folium
+import base64
+import json
+
+POPUP_WIDTH = 200
+
+image_html = '<img src="data:image/png;base64,{}" style="width:{}px;height:{}px;">'.format
+
+### GENERATE MAP ###
+
+def load_map(datafile, return_html=True):
+    """
+    Create an area map with boulder markers and video links from a JSON file
+    """
+    area_data = {}
+    with open(datafile) as data:
+        area_data = json.load(data)
+
+    area_map = folium.Map(location=[area_data['latitude'], area_data['longitude']],
+        zoom_start=area_data['zoom'])
+
+    # Parking
+    folium.Marker(
+        location=[area_data['parking_latitude'],area_data['parking_longitude']],
+        popup='Parking',
+        tooltip='Parking',
+        icon=folium.Icon(color='red', icon='info-sign')
+    ).add_to(area_map)
+
+    # Create and add boulder markers
+    boulders = area_data['boulders']
+    for boulder in boulders: 
+        boulder_image = image_html(base64.b64encode(open(boulder['image'], 'rb').read()).decode(), boulder['width'], boulder['height'])
+        boulder_lines = ['<br><a href="'+line['link']+'"target="_blank">'+line['name']+'</a>' for line in boulder['lines']]
+        lines_html = '<p> <b><u>'+ boulder['name'] +'</u></b><br>'+('').join(boulder_lines)+'</p>'
+        boulder_html = lines_html + '<p>' + boulder_image + '</p>'
+
+        boulder_popup = folium.Popup(boulder_html, max_width=POPUP_WIDTH, min_width=POPUP_WIDTH)
+
+        boulder_tooltip = boulder['name']
+
+        folium.Marker(
+            location=[boulder['latitude'],boulder['longitude']],
+            popup=boulder_popup,
+            tooltip=boulder_tooltip,
+            icon=folium.Icon(color='green', icon='info-sign')
+        ).add_to(area_map)
+
+    return area_map.get_root().render() if return_html else area_map
