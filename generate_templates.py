f8800c88ed04de59a9ab28e8f27a43f1bce917f0
Juan Gallostra <juangallostra@gmail.com>
2019-06-20 10:21:55 +0200

    Generate maps from new data structures

--- generate_templates.py
+++ generate_templates.py
@@ -3,10 +3,8 @@
 import load_map
 
 def main():
-	load_map.load_map("data/savassona.txt", False).save("templates/savassona.html")
-	load_map.load_map("data/sant_joan.txt", False).save("templates/sant_joan.html")
-#    sava.get_savassona_map(False).save("templates/savassona.html")
-#	 sant_joan.get_sant_joan_map(False).save("templates/sant_joan.html")
+	load_map.load_map("data/savassona/savassona.txt", False).save("templates/savassona.html")
+#	load_map.load_map("data/sant_joan.txt", None, False).save("templates/sant_joan.html")
 
 if __name__ == '__main__':
     main()
--- load_map.py
+++ load_map.py
@@ -1,11 +1,13 @@
 import numpy as np
 import folium
-import base64
+from folium.plugins import MarkerCluster
+# import base64
 import json
+import os
 
-POPUP_WIDTH = 200
+POPUP_WIDTH = 100
 
-image_html = '<img src="data:image/png;base64,{}" style="width:{}px;height:{}px;">'.format
+# image_html = '<img src="data:image/png;base64,{}" style="width:{}px;height:{}px;">'.format
 
 ### GENERATE MAP ###
 
@@ -20,6 +22,24 @@ def load_map(datafile, return_html=True):
     area_map = folium.Map(location=[area_data['latitude'], area_data['longitude']],
         zoom_start=area_data['zoom'])
 
+    sectors = area_data['sectors']
+    for sector in sectors:
+
+        sector_map = folium.GeoJson(
+            os.path.dirname(os.path.abspath(datafile))+sector['sector_data'],
+            name=sector['name'],
+            tooltip=sector['name'],
+            style_function=lambda x: {
+                'color' : x['properties']['stroke'],
+                'weight' : x['properties']['stroke-width'],
+                'opacity': 0.6,
+                'fillColor' : x['properties']['stroke'],
+        })
+        sector_html = '<p> <b><u>'+sector['name']+'</u></b><br><br><a href="'+sector['link']+'"target="_blank">Beta videos</a</p>'
+        sector_map.add_child(folium.Popup(sector_html,
+            max_width=POPUP_WIDTH, min_width=POPUP_WIDTH))
+        sector_map.add_to(area_map)
+
     # Parking
     folium.Marker(
         location=[area_data['parking_latitude'],area_data['parking_longitude']],
@@ -28,23 +48,25 @@ def load_map(datafile, return_html=True):
         icon=folium.Icon(color='red', icon='info-sign')
     ).add_to(area_map)
 
-    # Create and add boulder markers
-    boulders = area_data['boulders']
-    for boulder in boulders: 
-        boulder_image = image_html(base64.b64encode(open(boulder['image'], 'rb').read()).decode(), boulder['width'], boulder['height'])
-        boulder_lines = ['<br><a href="'+line['link']+'"target="_blank">'+line['name']+'</a>' for line in boulder['lines']]
-        lines_html = '<p> <b><u>'+ boulder['name'] +'</u></b><br>'+('').join(boulder_lines)+'</p>'
-        boulder_html = lines_html + '<p>' + boulder_image + '</p>'
+    # marker_cluster = MarkerCluster().add_to(area_map)
 
-        boulder_popup = folium.Popup(boulder_html, max_width=POPUP_WIDTH, min_width=POPUP_WIDTH)
+    # # Create and add boulder markers
+    # boulders = area_data['boulders']
+    # for boulder in boulders: 
+    #     boulder_image = image_html(base64.b64encode(open(boulder['image'], 'rb').read()).decode(), boulder['width'], boulder['height'])
+    #     boulder_lines = ['<br><a href="'+line['link']+'"target="_blank">'+line['name']+'</a>' for line in boulder['lines']]
+    #     lines_html = '<p> <b><u>'+ boulder['name'] +'</u></b><br>'+('').join(boulder_lines)+'</p>'
+    #     boulder_html = lines_html + '<p>' + boulder_image + '</p>'
 
-        boulder_tooltip = boulder['name']
+    #     boulder_popup = folium.Popup(boulder_html, max_width=POPUP_WIDTH, min_width=POPUP_WIDTH)
 
-        folium.Marker(
-            location=[boulder['latitude'],boulder['longitude']],
-            popup=boulder_popup,
-            tooltip=boulder_tooltip,
-            icon=folium.Icon(color='green', icon='info-sign')
-        ).add_to(area_map)
+    #     boulder_tooltip = boulder['name']
+
+    #     folium.Marker(
+    #         location=[boulder['latitude'],boulder['longitude']],
+    #         popup=boulder_popup,
+    #         tooltip=boulder_tooltip,
+    #         icon=folium.Icon(color='green', icon='info-sign')
+    #     ).add_to(marker_cluster)
 
     return area_map.get_root().render() if return_html else area_map
