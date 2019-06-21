b6e13833fc88fedef0caf103e5d0a9c0d68a22c1
Juan Gallostra <juangallostra@gmail.com>
2019-06-21 18:00:56 +0200

    Generate Sant Joan template also

--- generate_templates.py
+++ generate_templates.py
@@ -7,6 +7,8 @@ def main():
 	# load_map.load_map("data/savassona/savassona.txt", False).save("templates/savassona.html")
 	with open('templates/savassona.html', 'w') as template:
 	 	template.write(load_map.load_map("data/savassona/savassona.txt", True))
+	with open('templates/sant_joan.html', 'w') as template:
+	 	template.write(load_map.load_map("data/sant_joan/sant_joan.txt", True))
 
 if __name__ == '__main__':
     main()
