e2de56dba98d5d59848053de6c342d033009ceea
Juan Gallostra <juangallostra@gmail.com>
2019-06-21 18:01:20 +0200

    Enable hidding layer by default

--- helpers.py
+++ helpers.py
@@ -4,15 +4,19 @@ def generate_sector_html(name, link):
     Generate the html code tat shows the sector name and the link to the playlist
     when clicking on the sector area
     """
-    return '<p> <b><u>{}</u></b><br><a href="{}"target="_blank">Beta videos</a</p>'.format(name, link)
+    return '<p> <b><u>{}</u></b><br><br><a href="{}"target="_blank">Beta videos</a><br></p>'.format(name, link)
 
-def make_layer_that_hides(map_html, map_name, layer_name, zoom_level=15):
+def make_layer_that_hides(map_html, map_name, layer_name, zoom_level=15, visible=True, reverse=False):
     """
     Add a piece of JavaScript at the end of the map html that hides/shows
     a layer depending on the zoom level
     """
-    java = """        map_name.on('zoomend', function() {
-                if (map_name.getZoom() < zoom_level){
+    end_of_script = "\n\n</script>"
+    hide_by_default = """       map_name.removeLayer(layer_name);\n
+    """
+
+    code_to_inject = """        map_name.on('zoomend', function() {
+                if (map_name.getZoom() <= zoom_level){
                     map_name.removeLayer(layer_name);
                 }
                 else {
@@ -20,5 +24,10 @@ def make_layer_that_hides(map_html, map_name, layer_name, zoom_level=15):
                 }
         });"""
 
-    java = java.replace("map_name", map_name).replace("layer_name", layer_name).replace("zoom_level", str(zoom_level))
-    return map_html[:-9] + java + "\n\n</script>"
+    code_to_inject = code_to_inject.replace("map_name", map_name).replace("layer_name", layer_name).replace("zoom_level", str(zoom_level))
+    if reverse:
+        code_to_inject = code_to_inject.replace("<=", ">")
+    if not visible:
+        hide_by_default =  hide_by_default.replace("map_name", map_name).replace("layer_name", layer_name)
+        return map_html[:-9] + hide_by_default +  code_to_inject + end_of_script
+    return map_html[:-9] + code_to_inject + end_of_script
