fcf1418838274af53f229966ea275911a415efb8
Juan Gallostra <juangallostra@gmail.com>
2019-06-22 12:37:02 +0200

    Load favicon

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # import the Flask class from the flask module
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory, url_for
 
 # create the application object
 app = Flask(__name__)
@@ -16,6 +17,11 @@ def savassona():
 @app.route('/sant_joan')
 def sant_joan():
 	return render_template('sant_joan.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 # start the server with the 'run()' method
 if __name__ == '__main__':
