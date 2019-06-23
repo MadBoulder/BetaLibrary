06afd3012c013bb78e21d3522ad55f6ecf8dae89
Juan Gallostra <juangallostra@gmail.com>
2019-06-23 12:00:25 +0200

    Change location where generated maps are stored

--- generate_templates.py
+++ generate_templates.py
@@ -5,9 +5,9 @@ import load_map
 def main():
 	# load_map.load_map("data/sant_joan.txt", None, False).save("templates/sant_joan.html")
 	# load_map.load_map("data/savassona/savassona.txt", False).save("templates/savassona.html")
-	with open('templates/savassona.html', 'w') as template:
+	with open('templates/maps/savassona.html', 'w') as template:
 	 	template.write(load_map.load_map("data/savassona/savassona.txt", True))
-	with open('templates/sant_joan.html', 'w') as template:
+	with open('templates/maps/sant_joan.html', 'w') as template:
 	 	template.write(load_map.load_map("data/sant_joan/sant_joan.txt", True))
 
 if __name__ == '__main__':
