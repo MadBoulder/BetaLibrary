dcc1e86351a4946d1d6339dfab801098fe55d9a2
Juan Gallostra <juangallostra@gmail.com>
2019-07-09 20:13:18 +0200

    Move repeated parameter to a constant value

--- helpers.py
+++ helpers.py
@@ -1,4 +1,6 @@
 
+END_OF_SCRIPT = "\n\n</script>"
+
 def generate_sector_html(name, link):
     """
     Generate the html code tat shows the sector name and the link to the playlist
@@ -11,7 +13,6 @@ def make_layer_that_hides(map_html, map_name, layer_name, zoom_level=15, visible
     Add a piece of JavaScript at the end of the map html that hides/shows
     a layer depending on the zoom level
     """
-    end_of_script = "\n\n</script>"
     hide_by_default = "       map_name.removeLayer(layer_name);\n"
     code_to_inject = """        map_name.on('zoomend', function() {
                 if (map_name.getZoom() <= zoom_level){
@@ -27,16 +28,15 @@ def make_layer_that_hides(map_html, map_name, layer_name, zoom_level=15, visible
         code_to_inject = code_to_inject.replace("<=", ">")
     if not visible:
         hide_by_default =  hide_by_default.replace("map_name", map_name).replace("layer_name", layer_name)
-        return map_html[:-9] + hide_by_default +  code_to_inject + end_of_script
-    return map_html[:-9] + code_to_inject + end_of_script
+        return map_html[:-9] + hide_by_default +  code_to_inject + END_OF_SCRIPT
+    return map_html[:-9] + code_to_inject + END_OF_SCRIPT
 
 def zoom_on_click(map_html, map_name, marker_name, zoom_level):
     """
     """
-    end_of_script = "\n\n</script>"
     code_to_inject = """        marker_name.on('click', function(e){
             map_name.setView(e.latlng, zoom_level);
         });"""
 
     code_to_inject = code_to_inject.replace("map_name", map_name).replace("marker_name", marker_name).replace("zoom_level", str(zoom_level))
-    return map_html[:-9] + code_to_inject + end_of_script
+    return map_html[:-9] + code_to_inject + END_OF_SCRIPT
