728b8c12827d430bea7d21ef3f75f7ecb9cc04ca
Juan Gallostra Acín <jgallostraa@jgingenieros.es>
2019-08-19 10:57:06 +0200

    Cache latest videos query for 60 seconds

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask, render_template, send_from_directory, request
+from flask_caching import Cache
 import helpers
 
 # create the application object
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 # Load favicon
 @app.route('/favicon.ico')
@@ -25,6 +27,7 @@ def search():
 
 
 @app.route('/latest_videos')
+@cache.cached(timeout=60)
 def render_latest():
     return render_template('latest_videos.html', video_urls=helpers.get_videos_from_channel())
 
@@ -33,7 +36,7 @@ def render_latest():
 def render_page(page):
     return render_template(page + ".html")
 
-
+# this route is used for rendering maps inside an iframe
 @app.route('/maps/<string:area>')
 def render_area(area):
     return render_template('maps/' + area + ".html")
