a5c95a8832e7d9b4d2e8bc8c8bdc0e908c3f1c32
Juan Gallostra <juangallostra@gmail.com>
2019-08-29 18:28:40 +0200

    Comment unnecessary code and increase zoom limit to switch to marker

--- load_map.py
+++ load_map.py
@@ -8,7 +8,7 @@ import helpers
 
 POPUP_WIDTH = 100
 WIDTH_MULTIPLIER = 15
-DEFAULT_AREA_ZOOM = 14
+DEFAULT_AREA_ZOOM = 12
 SECTOR_OPACITY = 0.6
 MARKER_SIZE = 32
 PLACEHOLDER = '_placeholder'
@@ -96,8 +96,9 @@ def load_map(datafile, return_html=True):
 
 def load_general_map(datafiles, return_html=True):
     """
-    Create a map that contains all the info provided by the list of datafiles
-    i.e. all areas combined
+    Create a map that contains all the zones provided by the list of datafiles
+    i.e. all areas combined in one map. This map only shows the markers that 
+    indicate the 
     """
     area_map = folium.Map(location=[-33.046875, 66.51326044311185],
                           zoom_start=2)
@@ -115,41 +116,41 @@ def load_general_map(datafiles, return_html=True):
         with open(areadatafile) as data:
             area_data = json.load(data)
 
-        sectors = area_data['sectors']
-        # Create a Folium feature group for this layer, since we will be displaying multiple layers
-        sector_lyr = folium.FeatureGroup(name='sectors_layer')
-        for sector in sectors:
-            sector_map = folium.GeoJson(
-                os.path.dirname(os.path.abspath(areadatafile)) +
-                sector['sector_data'],
-                name=sector['name'],
-                tooltip=sector['name'],
-                style_function=lambda x: {
-                    'color': x['properties']['stroke'],
-                    'weight': x['properties']['stroke-width'],
-                    'opacity': SECTOR_OPACITY,
-                    'fillColor': x['properties']['stroke'],
-                }
-            )
-            sector_html = helpers.generate_sector_html(
-                sector['name'], sector['link'])
-            sector_map.add_child(folium.Popup(
-                sector_html, max_width=POPUP_WIDTH, min_width=POPUP_WIDTH))
+        # sectors = area_data['sectors']
+        # # Create a Folium feature group for this layer, since we will be displaying multiple layers
+        # sector_lyr = folium.FeatureGroup(name='sectors_layer')
+        # for sector in sectors:
+        #     sector_map = folium.GeoJson(
+        #         os.path.dirname(os.path.abspath(areadatafile)) +
+        #         sector['sector_data'],
+        #         name=sector['name'],
+        #         tooltip=sector['name'],
+        #         style_function=lambda x: {
+        #             'color': x['properties']['stroke'],
+        #             'weight': x['properties']['stroke-width'],
+        #             'opacity': SECTOR_OPACITY,
+        #             'fillColor': x['properties']['stroke'],
+        #         }
+        #     )
+        #     sector_html = helpers.generate_sector_html(
+        #         sector['name'], sector['link'])
+        #     sector_map.add_child(folium.Popup(
+        #         sector_html, max_width=POPUP_WIDTH, min_width=POPUP_WIDTH))
 
-            sector_lyr.add_child(sector_map)
+        #     sector_lyr.add_child(sector_map)
 
         # Parking
-        for parking in area_data['parkings']:
-            parking_marker = folium.Marker(
-                location=[parking['parking_latitude'],
-                          parking['parking_longitude']],
-                popup=helpers.generate_parking_html([parking['parking_latitude'],
-                                                     parking['parking_longitude']]),
-                tooltip='Parking',
-                icon=folium.Icon(color='red', icon='info-sign')
-            )
+        # for parking in area_data['parkings']:
+        #     parking_marker = folium.Marker(
+        #         location=[parking['parking_latitude'],
+        #                   parking['parking_longitude']],
+        #         popup=helpers.generate_parking_html([parking['parking_latitude'],
+        #                                              parking['parking_longitude']]),
+        #         tooltip='Parking',
+        #         icon=folium.Icon(color='red', icon='info-sign')
+        #     )
 
-            sector_lyr.add_child(parking_marker)
+        #     sector_lyr.add_child(parking_marker)
 
         zoomed_out_icon = CustomIcon(
             'static/images/marker/marker.png', icon_size=(MARKER_SIZE, MARKER_SIZE))
@@ -175,22 +176,22 @@ def load_general_map(datafiles, return_html=True):
         zoomed_out_lyr.add_child(areas_cluster)
 
         area_map.add_child(zoomed_out_lyr)
-        area_map.add_child(sector_lyr)
-        layers += [(sector_lyr, zoomed_out_lyr)]
+        # area_map.add_child(sector_lyr)
+        # layers += [(sector_lyr, zoomed_out_lyr)]
 
     # Since folium does not support all the functionalities we need
     # we obtain them by injecting or editing JavaScript code in the map html
     map_html = area_map.get_root().render()
-    for sector_lyr, zoomed_out_lyr in layers:
-        # Hide or show layers depending on the zoom level
-        map_html = helpers.make_layer_that_hides(
-            map_html, area_map.get_name(), sector_lyr.get_name(), DEFAULT_AREA_ZOOM, False, False)
-        map_html = helpers.make_layer_that_hides(
-            map_html, area_map.get_name(), zoomed_out_lyr.get_name(), DEFAULT_AREA_ZOOM, True, True)
-        # Zoom into area when clicking
-        # for marker in sectors_markers:
-        #     map_html = helpers.zoom_on_click(
-        #         map_html, area_map.get_name(), marker.get_name(), DEFAULT_AREA_ZOOM+1)
+    # for sector_lyr, zoomed_out_lyr in layers:
+    #     # Hide or show layers depending on the zoom level
+    #     map_html = helpers.make_layer_that_hides(
+    #         map_html, area_map.get_name(), sector_lyr.get_name(), DEFAULT_AREA_ZOOM, False, False)
+    #     map_html = helpers.make_layer_that_hides(
+    #         map_html, area_map.get_name(), zoomed_out_lyr.get_name(), DEFAULT_AREA_ZOOM, True, True)
+    #     # Zoom into area when clicking
+    #     for marker in sectors_markers:
+    #         map_html = helpers.zoom_on_click(
+    #             map_html, area_map.get_name(), marker.get_name(), DEFAULT_AREA_ZOOM+1)
     map_html = helpers.replace_custom_placeholders(map_html, placeholders)
     
     return map_html if return_html else area_map
