aedc93342f1e972452cce6297887b580409d5bb6
Juan Gallostra Acín <jgallostraa@jgingenieros.es>
2019-08-29 11:02:24 +0200

    When creating zone pages create a file with all playlists

--- generate_pages.py
+++ generate_pages.py
@@ -14,13 +14,16 @@ def main():
     template_loader = FileSystemLoader( searchpath="." )
     template_env = Environment( loader=template_loader )
 
+    playlists = {}
     for area in areas:
+        # Create zone map
         print(area)
         datafile = 'data/zones/' + area + '/' + area + '.txt'
         area_data = {}
         with open(datafile) as data:
             area_data = json.load(data)
 
+        playlists[area] = area_data['playlist']
         guides = [(guide['name'], guide['link']) for guide in area_data['guides']]
 
         template = template_env.get_template('templates/zone_layout.html')
@@ -28,6 +31,9 @@ def main():
         with open('templates/zones/'+area+'.html', 'w') as template:
             template.write(output)
 
+    # Update playlists file
+    with open('data/playlist.txt', 'w') as playlists_file:
+        playlists_file.write(json.dumps(playlists))
 
 if __name__ == '__main__':
     main()
