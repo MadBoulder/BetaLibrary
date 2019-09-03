53b252a35894fa34dda4ab7347352a5f12bcebf9
Juan Gallostra <juangallostra@gmail.com>
2019-09-03 17:41:10 +0200

    Add playlists to zone pages and reestructure page layout

--- generate_pages.py
+++ generate_pages.py
@@ -11,8 +11,8 @@ def main():
     areas = next(os.walk('data/zones/'))[1]
     all_data = ['data/zones/' + area + '/' + area + '.txt' for area in areas]
 
-    template_loader = FileSystemLoader( searchpath="." )
-    template_env = Environment( loader=template_loader )
+    template_loader = FileSystemLoader(searchpath=".")
+    template_env = Environment(loader=template_loader)
 
     playlists = {}
     for area in areas:
@@ -23,17 +23,26 @@ def main():
         with open(datafile) as data:
             area_data = json.load(data)
 
+        guides = [(guide['name'], guide['link'])
+                  for guide in area_data['guides']]
+
+        base_url = "https://www.youtube.com/embed/?listType=playlist&list="
         playlists[area] = area_data['playlist']
-        guides = [(guide['name'], guide['link']) for guide in area_data['guides']]
+        sectors_playlists = [(playlist['sector'], base_url + playlist['link'])
+                             for playlist in area_data['sectors_playlists']]
 
         template = template_env.get_template('templates/zone_layout.html')
-        output = template.render(name=area_data['name'], guide_list=guides, map_url='maps/'+area)
+        output = template.render(
+            name=area_data['name'], guide_list=guides,
+            map_url='maps/'+area, full_playlist=base_url + area_data['playlist'],
+            playlists=sectors_playlists)
         with open('templates/zones/'+area+'.html', 'w') as template:
             template.write(output)
 
     # Update playlists file
     with open('data/playlist.txt', 'w') as playlists_file:
         playlists_file.write(json.dumps(playlists))
+
 
 if __name__ == '__main__':
     main()
