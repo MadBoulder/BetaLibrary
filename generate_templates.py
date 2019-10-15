f219f4228ef830c6e642cc90ae9a29f22dc0fdbb
Juan Gallostra Acín <jgallostraa@jgingenieros.es>
2019-10-15 14:40:18 +0200

    Update all template when generating templates

--- generate_templates.py
+++ generate_templates.py
@@ -1,5 +1,7 @@
 import load_map
 import os
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import helpers
 
 
 def main():
@@ -18,6 +20,17 @@ def main():
     with open('templates/maps/all_to_render.html', 'w', encoding='utf-8') as template:
         template.write(load_map.load_general_map(
             all_data, True))
+
+    # When generating templates update also the all template
+    template_loader = FileSystemLoader(searchpath=".")
+    template_env = Environment(loader=template_loader)
+    data = helpers.get_number_of_videos_from_playlists_file(
+        'data/playlist.txt')
+    template = template_env.get_template('templates/maps/all_to_render.html')
+    # Here we replace zone_name in maps/all by the number of beta videos
+    output = template.render(**data)
+    with open('templates/maps/all.html', 'w', encoding="utf-8") as template:
+        template.write(output)
 
 
 if __name__ == '__main__':
