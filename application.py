70f0703ee75bfa2c0ca6fd056b20a724c623d2e7
Juan Gallostra Acín <jgallostraa@jgingenieros.es>
2020-01-07 12:38:03 +0100

    Replace placeholders when rendering

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ from flask_babel import Babel, _
 from flask_mail import Mail,  Message
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import helpers
+import js_helpers
 from werkzeug.utils import secure_filename
 
 
@@ -55,6 +56,8 @@ def get_map_all():
     template = template_env.get_template('templates/maps/all_to_render.html')
     # Here we replace zone_name in maps/all by the number of beta videos
     output = template.render(**data)
+    output = js_helpers.replace_sectors_placeholders_for_translations(
+        output)
     with open('templates/maps/all.html', 'w', encoding="utf-8") as template:
         template.write(output)
 
