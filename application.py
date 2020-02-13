7e2b77c96ea6652af1b5bfbf6acf5aecfc6f8795
Aniol Santacreu <asantacreu@twonav.com>
2020-02-13 13:35:47 +0100

    Fixes erased email

--- application.py
+++ application.py
@@ -25,7 +25,10 @@ mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD'],
+    "MAIL_RECIPIENTS": os.environ['EMAIL_RECIPIENTS'].split(":")
 }
 
 app.config.update(mail_settings)
