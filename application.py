4d62601f6c8ee05de73ee0d986bef6e5db42a233
Aniol Santacreu <asantacreu@twonav.com>
2020-02-13 12:39:03 +0100

    Adds info in Alcaniz zone

--- application.py
+++ application.py
@@ -25,10 +25,7 @@ mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
-    "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": os.environ['EMAIL_USER'],
-    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD'],
-    "MAIL_RECIPIENTS": os.environ['EMAIL_RECIPIENTS'].split(":")
+    "MAIL_USE_SSL": True
 }
 
 app.config.update(mail_settings)
