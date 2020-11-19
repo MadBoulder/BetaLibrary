008fba70f4341c79b668ac80eec5eaf8f2582a34
Juan Gallostra <juangallostra@gmail.com>
2020-11-19 09:31:06 +0100

    Add countries list

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ from flask_babel import _
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = [_('at'), _('de'), _('za'), _('fr'), _('ca'), _('it'), _('es'), _('ch'), _('us')]
+COUNTRIES = [_('Austria'), _('Germany'), _('France'), _('United States of America'), _('Spain'), _('Switzerland'), _('South Africa'), _('Canada'), _('Italy')]
