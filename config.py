bb8608662ea8a7eeb0d86837e84dee38d43a1b1e
Juan Gallostra <juangallostra@gmail.com>
2020-11-26 09:13:22 +0100

    Remove unnecessary import

--- config.py
+++ config.py
@@ -1,6 +1,3 @@
-
-from flask_babel import _
-
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
