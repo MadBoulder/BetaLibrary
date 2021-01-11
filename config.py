99c325aac731868abce36f938a8576ef0c987e2f
Juan Gallostra <juangallostra@gmail.com>
2021-01-11 12:48:14 +0100

    Update Hampi data

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['za', 'es', 'at', 'ca', 'it', 'fr', 'in', 'de', 'ch', 'us']
+COUNTRIES = ['fr', 'za', 'in', 'ch', 'us', 'ca', 'de', 'es', 'at', 'it']
