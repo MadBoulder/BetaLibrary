715dbc6d8a5fba465c0f8709ea169109030f952f
Juan Gallostra <juangallostra@gmail.com>
2021-01-11 13:01:47 +0100

    Add Black Mountain map and page

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['fr', 'za', 'in', 'ch', 'us', 'ca', 'de', 'es', 'at', 'it']
+COUNTRIES = ['fr', 'it', 'us', 'ch', 'in', 'za', 'ca', 'at', 'es', 'de']
