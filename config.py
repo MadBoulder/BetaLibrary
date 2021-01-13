82d296e983354397fc7b6302ca893ec4bb11cad2
Juan Gallostra <juangallostra@gmail.com>
2021-01-13 11:33:34 +0100

    Update banyoles data

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['ch', 'ca', 'in', 'it', 'za', 'de', 'us', 'fr', 'at', 'es']
+COUNTRIES = ['za', 'fr', 'it', 'de', 'ca', 'at', 'in', 'es', 'ch', 'us']
