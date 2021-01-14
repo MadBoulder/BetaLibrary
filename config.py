93b92f68b76ae4686e709eacd0094925fa390681
Juan Gallostra <juangallostra@gmail.com>
2021-01-14 11:42:23 +0100

    Update pages and maps

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['za', 'fr', 'it', 'de', 'ca', 'at', 'in', 'es', 'ch', 'us']
+COUNTRIES = ['at', 'ch', 'de', 'es', 'ca', 'in', 'us', 'za', 'it', 'fr']
