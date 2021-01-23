145992bd344b77dd0da2d58cd7d3a234935e577b
Juan Gallostra <juangallostra@gmail.com>
2021-01-23 14:12:23 +0100

    Add Vilajuïga official instagram

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['es', 'za', 'ch', 'de', 'it', 'in', 'fr', 'ca', 'at', 'us']
+COUNTRIES = ['it', 'de', 'ca', 'in', 'za', 'fr', 'us', 'at', 'es', 'ch']
