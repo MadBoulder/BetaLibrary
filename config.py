20118454c3521cffc9fe1edaa6a9712861dc3fff
Juan Gallostra <juangallostra@gmail.com>
2021-01-23 14:09:39 +0100

    Add official Savassona instagram

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['es', 'in', 'us', 'it', 'at', 'fr', 'de', 'za', 'ca', 'ch']
+COUNTRIES = ['it', 'de', 'za', 'in', 'at', 'ch', 'us', 'ca', 'fr', 'es']
