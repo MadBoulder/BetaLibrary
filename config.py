3ec181d5300ca87645f9e8383c4ae8b5574c0ec7
Juan Gallostra <juangallostra@gmail.com>
2021-01-31 20:11:05 +0100

    Remove Mañaria

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['ch', 'fr', 'de', 'it', 'za', 'ca', 'in', 'us', 'at', 'es']
+COUNTRIES = ['de', 'es', 'za', 'ca', 'fr', 'it', 'us', 'ch', 'at', 'in']
