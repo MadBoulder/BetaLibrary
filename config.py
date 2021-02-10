4437739534619780c9e2f6bcc9b83394da570fba
Juan Gallostra <juangallostra@gmail.com>
2021-02-10 12:04:10 +0100

    Add new sector to Salvanebleau

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['ca', 'at', 'it', 'us', 'de', 'in', 'za', 'fr', 'es', 'ch']
+COUNTRIES = ['za', 'fr', 'in', 'us', 'ch', 'ca', 'it', 'es', 'at', 'de']
