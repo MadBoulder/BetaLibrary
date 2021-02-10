46b3c78a1d5e661d554727ffc5ee5296dc45362d
Juan Gallostra <juangallostra@gmail.com>
2021-02-10 11:43:56 +0100

    Add Jaizkibel

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['de', 'es', 'za', 'ca', 'fr', 'it', 'us', 'ch', 'at', 'in']
+COUNTRIES = ['za', 'in', 'ca', 'us', 'es', 'ch', 'fr', 'at', 'de', 'it']
