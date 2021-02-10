9f07e7be9107bc5983da41794e60661e24e3717a
Juan Gallostra <juangallostra@gmail.com>
2021-02-10 11:47:48 +0100

    Add new sector to Bezas

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['za', 'in', 'ca', 'us', 'es', 'ch', 'fr', 'at', 'de', 'it']
+COUNTRIES = ['ca', 'in', 'us', 'de', 'za', 'es', 'at', 'ch', 'it', 'fr']
