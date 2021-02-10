078bbd3eddb2f72c925eed5e051c0d6cd58dd7ea
Juan Gallostra <juangallostra@gmail.com>
2021-02-10 11:54:44 +0100

    Add new sector to Targassone

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['fr', 'de', 'us', 'at', 'ca', 'ch', 'in', 'za', 'it', 'es']
+COUNTRIES = ['ca', 'at', 'fr', 'de', 'us', 'es', 'in', 'it', 'ch', 'za']
