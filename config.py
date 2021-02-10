b01225e3841498047e322d7313731fd881c87a8d
Juan Gallostra <juangallostra@gmail.com>
2021-02-10 11:49:53 +0100

    Add new sector to Alcañiz

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['ca', 'in', 'us', 'de', 'za', 'es', 'at', 'ch', 'it', 'fr']
+COUNTRIES = ['us', 'fr', 'de', 'in', 'es', 'it', 'at', 'za', 'ch', 'ca']
