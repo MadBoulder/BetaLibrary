7237ed29d8d1a717702dc8986c8fbd88b73a8ac2
Juan Gallostra <juangallostra@gmail.com>
2021-02-10 11:53:14 +0100

    Add new sector to Albarracín

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['fr', 'at', 'de', 'ch', 'ca', 'us', 'in', 'es', 'za', 'it']
+COUNTRIES = ['fr', 'de', 'us', 'at', 'ca', 'ch', 'in', 'za', 'it', 'es']
