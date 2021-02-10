9b664f1dc0166ef3bb6d3d02b5d53064af169108
Juan Gallostra <juangallostra@gmail.com>
2021-02-10 11:58:40 +0100

    Add new sector to La pedriza

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['ca', 'at', 'fr', 'de', 'us', 'es', 'in', 'it', 'ch', 'za']
+COUNTRIES = ['ca', 'at', 'it', 'us', 'de', 'in', 'za', 'fr', 'es', 'ch']
