f371a557fcd28398f9069cd164a0e2b8d60127c7
Juan Gallostra <juangallostra@gmail.com>
2021-02-10 11:51:40 +0100

    Add new sector to font

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
-COUNTRIES = ['us', 'fr', 'de', 'in', 'es', 'it', 'at', 'za', 'ch', 'ca']
+COUNTRIES = ['fr', 'at', 'de', 'ch', 'ca', 'us', 'in', 'es', 'za', 'it']
