959d54f886a6a2e09612866d61b6989895abd7a1
Juan Gallostra <juangallostra@gmail.com>
2021-11-23 08:25:34 +0100

    Improve styles

--- dashboard.py
+++ dashboard.py
@@ -2,14 +2,13 @@ from os.path import dirname, join
 import json
 import collections
 import math
-from datetime import date, datetime
 
 import numpy as np
 import pandas as pd
 
 from bokeh.layouts import column, layout
 from bokeh.models import ColumnDataSource, Div, Select, Slider, HoverTool, RangeSlider
-from bokeh.models import  RadioButtonGroup, DataTable, TableColumn, CheckboxGroup
+from bokeh.models import RadioButtonGroup, DataTable, TableColumn, CheckboxGroup
 from bokeh.plotting import figure
 
 from bokeh.models.callbacks import CustomJS
