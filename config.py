a448e63f98c450728ecd9aac1ab81ee2aef76911
Juan Gallostra <juangallostra@gmail.com>
2022-04-15 14:36:55 +0200

    Add sectors to albarracin and la jonquera

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
 UPLOADED_FILES = "https://script.googleusercontent.com/macros/echo?user_content_key=mwFC7Ng8Lhk4zfO9ZhZx2AWa1oLV6vaBB7DRBAY4dfSu8Gxn923WpDuCDH1F3RddApP-JytKgX_hzMaLStxmDKuH3UXIUmnSm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCtYMMby20j7b-NxgzPeYTNED0HXXmJVgJlmlfr3Zfayorzuj5gTcw58hm8b34bXH51sckVbt7XqGGaU2pP6Ntk&lib=MtwnoRVXTHrAxMLFARDgn1TMbC6R9xjvU"
-COUNTRIES = ['ch', 'ca', 'se', 'za', 'es', 'de', 'at', 'fr', 'it', 'gb', 'in', 'us']
+COUNTRIES = ['ch', 'de', 'it', 'at', 'fr', 'gb', 'in', 'se', 'us', 'za', 'es', 'ca']
