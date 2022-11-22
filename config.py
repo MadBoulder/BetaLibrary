fd422dfbc0fd21330c68104e9d50e7909078be52
Juan Gallostra <juangallostra@gmail.com>
2022-11-22 08:36:10 +0100

    add meschia nuova to zones

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
 UPLOADED_FILES = "https://script.googleusercontent.com/macros/echo?user_content_key=mwFC7Ng8Lhk4zfO9ZhZx2AWa1oLV6vaBB7DRBAY4dfSu8Gxn923WpDuCDH1F3RddApP-JytKgX_hzMaLStxmDKuH3UXIUmnSm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCtYMMby20j7b-NxgzPeYTNED0HXXmJVgJlmlfr3Zfayorzuj5gTcw58hm8b34bXH51sckVbt7XqGGaU2pP6Ntk&lib=MtwnoRVXTHrAxMLFARDgn1TMbC6R9xjvU"
-COUNTRIES = ['fr', 'za', 'es', 'us', 'ca', 'se', 'in', 'ar', 'gb', 'de', 'pt', 'at', 'ch', 'br', 'it']
+COUNTRIES = ['in', 'gb', 'ch', 'us', 'at', 'br', 'se', 'ar', 'ca', 'za', 'pt', 'it', 'fr', 'de', 'es']
