1f022c64859d33cbb1221e8f648b271791fd4a09
Aniol Santacreu <aniolsantacreusalva@gmail.com>
2023-06-17 06:30:47 +0200

    Revert application

--- application.py
+++ application.py
@@ -30,16 +30,16 @@ app.secret_key = b'\xf7\x81Q\x89}\x02\xff\x98<et^'
 babel = Babel(app)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
-# mail_settings = {
-#     "MAIL_SERVER": 'smtp.gmail.com',
-#     "MAIL_PORT": 465,
-#     "MAIL_USE_TLS": False,
-#     "MAIL_USE_SSL": True,
-#     "MAIL_USERNAME": os.environ['EMAIL_USER'],
-#     "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD'],
-#     "MAIL_RECIPIENTS": os.environ['EMAIL_RECIPIENTS'].split(":"),
-#     "FEEDBACK_MAIL_RECIPIENTS": os.environ['FEEDBACK_MAIL_RECIPIENTS'].split(":")
-# }
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ['EMAIL_USER'],
+    "MAIL_PASSWORD": os.environ['EMAIL_PASSWORD'],
+    "MAIL_RECIPIENTS": os.environ['EMAIL_RECIPIENTS'].split(":"),
+    "FEEDBACK_MAIL_RECIPIENTS": os.environ['FEEDBACK_MAIL_RECIPIENTS'].split(":")
+}
 
 # app.config.update(mail_settings)
 # mail = Mail(app)
@@ -113,6 +113,7 @@ def set_language(language=None):
     else:
         return redirect('')
 
+@babel.localeselector
 def get_locale():
     # if the user has set up the language manually it will be stored in the session,
     # so we use the locale from the user settings
@@ -123,8 +124,6 @@ def get_locale():
     if language is not None:
         return language
     return 'en'
-babel.init_app(app, locale_selector=get_locale)
-
 
 
 # Load favicon
