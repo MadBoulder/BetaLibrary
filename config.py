9076344973aaf406e66e99f920fe18c7f020b28f
Juan Gallostra <juangallostra@gmail.com>
2023-07-06 11:42:30 +0200

    add new zones

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
 UPLOADED_FILES = "https://script.googleusercontent.com/macros/echo?user_content_key=mwFC7Ng8Lhk4zfO9ZhZx2AWa1oLV6vaBB7DRBAY4dfSu8Gxn923WpDuCDH1F3RddApP-JytKgX_hzMaLStxmDKuH3UXIUmnSm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCtYMMby20j7b-NxgzPeYTNED0HXXmJVgJlmlfr3Zfayorzuj5gTcw58hm8b34bXH51sckVbt7XqGGaU2pP6Ntk&lib=MtwnoRVXTHrAxMLFARDgn1TMbC6R9xjvU"
-COUNTRIES = ['gb', 'it', 'us', 'za', 'pt', 'fr', 'se', 'br', 'de', 'at', 'es', 'ca', 'in', 'ar', 'ch']
+COUNTRIES = ['in', 'pt', 'de', 'ar', 'it', 'se', 'br', 'fr', 'ca', 'gb', 'za', 'ad', 'es', 'at', 'ch', 'us']
