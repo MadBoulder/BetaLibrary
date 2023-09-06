79840d81c606cf4a269a5abc9b2814999d8243f2
Aniol Santacreu <aniolsantacreusalva@gmail.com>
2023-09-06 08:25:05 +0200

    Renames pdfs and updates sitemaps

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ LANGUAGES = ['en', 'es', 'ca']
 WE_TRANSFER_LOCALE_MAPPING = {'en': 'en', 'es': 'es', 'ca': 'en'}
 ZONE_FILTERS = ['albarracin', 'albarracín', 'bezas']
 UPLOADED_FILES = "https://script.googleusercontent.com/macros/echo?user_content_key=mwFC7Ng8Lhk4zfO9ZhZx2AWa1oLV6vaBB7DRBAY4dfSu8Gxn923WpDuCDH1F3RddApP-JytKgX_hzMaLStxmDKuH3UXIUmnSm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnCtYMMby20j7b-NxgzPeYTNED0HXXmJVgJlmlfr3Zfayorzuj5gTcw58hm8b34bXH51sckVbt7XqGGaU2pP6Ntk&lib=MtwnoRVXTHrAxMLFARDgn1TMbC6R9xjvU"
-COUNTRIES = ['fr', 'br', 'gb', 'us', 'pt', 'es', 'cz', 'in', 'ch', 'za', 'ar', 'it', 'at', 'se', 'ca', 'mx', 'de', 'ad']
+COUNTRIES = ['se', 'ad', 'cz', 'us', 'es', 'it', 'ca', 'in', 'mx', 'ar', 'at', 'br', 'gb', 'pt', 'ch', 'za', 'de', 'fr']
